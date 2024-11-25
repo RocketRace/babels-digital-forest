3b311bc01bb6f089cc1e0e470c87a331475af492
RocketRace <oliviaspalmu@gmail.com>
2024-11-26 00:41:55 +0200

    playground

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,9 @@
 "use strict";
+const container = document.querySelector('#container');
+if (container) {
+    for (let i = 0; i < 1000; i++) {
+        const img = document.createElement('img');
+        img.setAttribute('src', `http://localhost:3000/img/${i}`);
+        container.appendChild(img);
+    }
+}
